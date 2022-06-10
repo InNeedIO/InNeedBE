73efd3d4e4ae2f40fed42b6415482c210d738234
Oskar Juszczyk <oskar.juszczyk@protonmail.com>
2022-06-10 16:02:56 +0200

    feat: remove migration

--- qa/token_bearer.py
+++ qa/token_bearer.py
@@ -1,3 +1,3 @@
 
 def bearer_token():
-    return "Bearer {token}"
+    return "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjU0NzE3NDYyLCJleHAiOjE2NTQ4OTAyNjJ9.4RHSpaIXd1WUls7JKxCNKZy9MKk_2v1Nb2Mc34353Eg"
